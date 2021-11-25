b2bb6d04193c8a3f49cbff69c98fffcef69cd79c
PrgoramaThor <felipe.castaneda84@gmail.com>
2021-11-24 23:12:51 -0300

    creado cachipun

---
+++ js/app.js
@@ -0,0 +1,66 @@
+var juego = prompt("¿Cuantas veces quieres jugar?");
+var respuesta = prompt("Elige: \nPiedra \nPapel \nTijera ");
+var compu =Math.floor( Math.random()*3);
+var _mensaje = "";
+var eleccion;
+if(respuesta== "Piedra"){
+    eleccion = 0;
+}else if(respuesta== "Papel"){
+    eleccion = 1;
+}else if(respuesta== "Tijera"){
+    eleccion = 2;
+}
+var elecompu;
+if(compu == 0){
+    elecompu = "Piedra";
+}else if(compu == 1){
+    elecompu = "Papel";
+}else if(compu == 2){
+    elecompu = "Tijera";
+}
+for(i=1; i<=juego; i++){
+    if(eleccion == compu){
+    _mensaje = "EMPATE";
+    alert(_mensaje)
+    document.write("Juego N°" +i+" tu sacaste "+respuesta+" y la compu sacó "+elecompu+".<br>");
+    }else {
+        if(eleccion==0){
+            if(compu == 1){
+            _mensaje = "Perdiste!";
+            document.write("Juego N°" +i+" tu sacaste "+respuesta+" y la compu sacó "+elecompu+".<br>");
+            }
+            if(compu == 2){
+            _mensaje = "Ganaste!";
+            document.write("Juego N°" +i+" tu sacaste "+respuesta+" y la compu sacó "+elecompu+".<br>");
+            }
+            alert(_mensaje)
+        }
+        if(eleccion==1){
+            if(compu == 0){
+            _mensaje = "Ganaste!";
+            document.write("Juego N°" +i+" tu sacaste "+respuesta+" y la compu sacó "+elecompu+".<br>");
+            }
+            if(compu == 2){
+            _mensaje = "Perdiste!";
+            document.write("Juego N°" +i+" tu sacaste "+respuesta+" y la compu sacó "+elecompu+".<br>");
+            }
+            alert(_mensaje)
+        }
+        if(eleccion==2){
+            if(compu == 0){
+            _mensaje = "Perdiste!";
+            document.write("Juego N°" +i+" tu sacaste "+respuesta+" y la compu sacó "+elecompu+".<br>");
+            }
+            if(compu == 1){
+            _mensaje = "Perdiste!";
+            document.write("Juego N°" +i+" tu sacaste "+respuesta+" y la compu sacó "+elecompu+".<br>");
+            }
+            alert(_mensaje)
+        }
+
+
+    }    
+
+
+}
+    
